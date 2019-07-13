3493d69275988179f9c91d9397153148e94a4390
Ahmed Kamal Arshad <52777845+ahmedkamal-TeraByte@users.noreply.github.com>
2019-07-13 10:32:14 +0500

    Created New Project

---
+++ UGPC-IIUI/UGPC-IIUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UGPC_IIUI.MvcApplication" Language="C#" %>
---
+++ UGPC-IIUI/UGPC-IIUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UGPC_IIUI.Startup))]
+namespace UGPC_IIUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
